f1846d3ea697de2ee97fc2dab6887c7bd903a03f
raviASUS2017 <ravindu.hasanka90@gmail.com>
2024-05-15 13:59:35 +0300

    add validMove function

--- knight.js
+++ knight.js
@@ -81,13 +81,165 @@ function moveSet() {
 }
 
 
+class Queue {
+    constructor() {
+        this.items = {}
+        this.frontIndex = 0
+        this.backIndex = 0
+    }
+    enqueue(item) {
+        this.items[this.backIndex] = item
+        this.backIndex++
+        return item + ' inserted'
+    }
+    dequeue() {
+        const item = this.items[this.frontIndex]
+        delete this.items[this.frontIndex]
+        this.frontIndex++
+        return item
+    }
+    peek() {
+        return this.items[this.frontIndex]
+    }
+    isEmpty() {
+        return Object.keys(this.items).length == 0;
+    }
+    get printQueue() {
+        return this.items;
+    }
+}
+
+// function levelOrder(root) {
+//     if (!root) return
+//     let fifo = new Queue;
+//     let visited = [];
+//     fifo.enqueue(root);
+//     while(fifo.isEmpty() !== true) {
+//         let node = fifo.peek();
+//         console.log(node.data, "level order");
+//         if (node.left != null) {
+//             fifo.enqueue(node.left)
+//         }
+//         if (node.right != null) {
+//             fifo.enqueue(node.right)
+//         }
+//         fifo.dequeue();
+//         // coolDown = coolDown - 1;
+//     }
+// }
+
+// function levelOrder(graph, start, end) {
+//     if (!start) return
+//     let queue = new Queue;
+//     let visited = new Array(graph.length).fill(false);
+//     queue.enqueue(start);
+//     // let visited = [];
+//     // visited.push(start);
+//     // let cooldown = 10;
+
+//     // while(queue && cooldown > 0) {
+//     while(queue.isEmpty() !== true) {
+//         s = queue.dequeue();
+//         values = graph[s];
+//         // console.log(s, "s", values);
+
+//         for (const n of values) {
+//             if (!visited[n]) {
+//                 // console.log(n)
+//                 // visited.push(n);
+//                 visited[n] = true;
+//                 queue.enqueue(n);
+//             }
+
+//             if (visited[end] == true) {
+//                 console.log("found it", queue.items);
+//             }
+//         }
+
+//     }
+
+//     // console.log(visited, visited[0]);
+
+//     // return queue
+//     return visited
+
+//     // while(queue.isEmpty() !== true && cooldown > 0) {
+//     //     let node = queue.peek();
+//     //     console.log(node, "level order");
+//     //     visited.push(node);
+//     //     if (moveList[node] !== null) {
+//     //         queue.enqueue(moveList[node][0])
+//     //     }
+//     //     queue.dequeue();
+//     //     // cooldown--;
+//     // }
+// }
+
+function levelOrder(graph, start, end) {
+    if (!start) return
+    let queue = new Queue;
+    let visited = new Array(graph.length).fill(null);
+    visited[start] = -1;
+    queue.enqueue(start);
+    let off = true;
+
+    while(queue.isEmpty() !== true && off == true) {
+        let current = queue.dequeue();
+        let values = graph[current];
+
+        for (const n of values) {
+            // if (visited[end] == true) {
+            //     console.log("found it", queue.items);
+            //     off = false;
+            //     break
+            // }
+
+            if (visited[n] === null) {
+                visited[n] = current;
+                queue.enqueue(n);
+            }
+
+            if (n === end) {
+                off = false;
+                break;
+            }
+        }
+    }
+
+    return visited
+
+}
+
 function knightMoves() {
 
 }
 
-console.log(validMoves([0,5]))
+// console.log(validMoves([0,5]))
 
 const possibleMoves = moveSet();
-console.log(possibleMoves);
+// console.log(possibleMoves);
 
-console.log("7,7")
+// console.log("7,7")
+
+// levelOrder('3,3', '5,5', possibleMoves);
+
+let testSet = new Array(possibleMoves.length).fill(false);
+
+
+// KEYS
+// console.log(testSet, Object.keys(possibleMoves));
+
+let check = '3,3'
+if (possibleMoves[check] === true) {
+    console.log("yes")
+}
+
+// console.log(possibleMoves[check], possibleMoves[check].length > 0 , check, "check")
+
+let result = levelOrder(possibleMoves, '3,3', '5,5');
+console.log(result)
+console.log(possibleMoves[check])
+// console.log(result['7,7']);
+// for (const n in possibleMoves[check]) {
+//     console.log(possibleMoves[check][n], check)
+// }
